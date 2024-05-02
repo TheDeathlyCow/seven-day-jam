be5bac1d32e293c053b10d1df1485a6288852566
TheDeathlyCow <thedeathlycow@gmail.com>
2024-05-01 23:34:01 -1000

    control platform/ship with p

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var sensitivity: float = 0.005
 
-@export var ship: Node3D
+@export var ship: CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -28,29 +28,23 @@ func _exit_tree():
 
 func _physics_process(delta):
 	
-	if is_piloting:
-		return
+	var controlled_body = get_controlled_body()
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not is_piloting:
+		process_jump(delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		controlled_body.velocity.x = direction.x * SPEED
+		controlled_body.velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		controlled_body.velocity.x = move_toward(velocity.x, 0, SPEED)
+		controlled_body.velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+	controlled_body.move_and_slide()
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -66,3 +60,20 @@ func rotate_camera(relative: Vector2):
 	transform.basis = Basis()
 	rotate_object_local(Vector3.UP, accumulated_rotation.x)
 	rotate_object_local(Vector3.RIGHT, accumulated_rotation.y)
+
+
+func process_jump(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+func get_controlled_body() -> CharacterBody3D:
+	if is_piloting:
+		return ship
+	else:
+		return self
